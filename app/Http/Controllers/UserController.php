bcbdbb8d46ddbc8909fbdf2fe6889e279aa97c5a
Prayogo <riotriprayogo31@gmail.com>
2025-03-03 11:14:04 +0700

    refactor: clean up UserController and UserModel by commenting out unused code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,10 +18,25 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data);
 
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data);
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
