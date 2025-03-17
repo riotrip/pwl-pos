66cd6577f9bd16d47ba10bfe71e21aa5cdcfbe03
Prayogo <riotriprayogo31@gmail.com>
2025-03-17 07:05:32 +0700

    feat: enhance user index view with level filter and update controller to pass level data

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -17,6 +17,29 @@
         @if (session('error'))
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select
+                            class="form-control"
+                            id="level_id"
+                            name="level_id"
+                            required
+                        >
+                            <option value="">- Semua -</option>
+                            @foreach ($level as $item)
+                                <option value="{{ $item->level_id }}"
+                                    >{{ $item->level_nama }}</option
+                                >
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Level Pengguna</small>
+                    </div>
+                </div>
+            </div>
+        </div>
         <table
             class="table table-bordered table-striped table-hover table-sm"
             id="table_user"
@@ -45,6 +68,9 @@
                 type: "POST",
                 headers: {
                     'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+                data: function (d) {
+                    d.level_id = $("#level_id").val();
                 },
             },
             columns: [
@@ -84,6 +110,10 @@
                 },
             ],
         });
+
+        $('#level_id').on('change', function () {
+            dataUser.ajax.reload();
+        })
     });
 </script>
 @endpush
