b15b43a36aa53a7cb06106e59106844252155753
Prayogo <riotriprayogo31@gmail.com>
2025-04-21 07:40:36 +0700

    feat: add import and export button for kategori, level, and supplier

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -3,11 +3,10 @@
     <div class="card-header">
         <h3 class="card-title">{{ $page->title }}</h3>
         <div class="card-tools">
-            <a
-                class="btn btn-sm btn-primary mt-1"
-                href="{{ url('kategori/create') }}"
-                >Tambah</a
-            >
+             <button onclick="modalAction('{{ url('/kategori/import') }}')" class="btn btn-sm btn-info mt-1">Import Kategori</button>
+            {{-- <a class="btn btn-sm btn-primary mt-1" href="{{ url('kategori/create') }}">Tambah</a> --}}
+            <a href="{{ url('/kategori/export_excel') }}" class="btn btn-sm btn-primary mt-1"><i class="fa fa-file-excel"></i> Export Kategori</a>
+            <a href="{{ url('/kategori/export_pdf') }}" class="btn btn-sm btn-warning mt-1"><i class="fa fa-file-pdf"></i> Export Kategori</a>
             <button class="btn btn-sm btn-success mt-1" onclick="modalAction('{{ url('kategori/create_ajax') }}')">Tambah Ajax</button>
         </div>
     </div>
--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -3,11 +3,10 @@
     <div class="card-header">
         <h3 class="card-title">{{ $page->title }}</h3>
         <div class="card-tools">
-            <a
-                class="btn btn-sm btn-primary mt-1"
-                href="{{ url('supplier/create') }}"
-                >Tambah</a
-            >
+            <button onclick="modalAction('{{ url('/supplier/import') }}')" class="btn btn-sm btn-info mt-1">Import Supplier</button>
+            {{-- <a class="btn btn-sm btn-primary mt-1" href="{{ url('supplier/create') }}">Tambah</a> --}}
+            <a href="{{ url('/supplier/export_excel') }}" class="btn btn-sm btn-primary mt-1"><i class="fa fa-file-excel"></i> Export Supplier</a>
+            <a href="{{ url('/supplier/export_pdf') }}" class="btn btn-sm btn-warning mt-1"><i class="fa fa-file-pdf"></i> Export Supplier</a>
             <button class="btn btn-sm btn-success mt-1" onclick="modalAction('{{ url('supplier/create_ajax') }}')">Tambah Ajax</button>
         </div>
     </div>
