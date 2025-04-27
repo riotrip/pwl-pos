87726b162a6d5fd0b4872f0743fd13f94dfecb0f
Prayogo <riotriprayogo31@gmail.com>
2025-04-28 05:01:57 +0700

    feat: add LevelController with CRUD operations for levels

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Monolog\Level;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +23,12 @@ Route:: middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/logout', \App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+Route::get('/levels', [LevelController::class, 'index']);
+Route::post('/levels', [LevelController::class, 'store']);
+Route::get('/levels/{level}', [LevelController::class, 'show']);
+Route::put('/levels/{level}', [LevelController::class, 'update']);
+Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
