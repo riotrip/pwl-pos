f0a22e9cb556ca146af80fcda0f07c3e3d49986a
Prayogo <riotriprayogo31@gmail.com>
2025-04-28 04:25:19 +0700

    feat: implement logout functionality with JWT token invalidation

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::post('/login', \App\Http\Controllers\Api\LoginController::class)->name('l
 Route:: middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', \App\Http\Controllers\Api\LogoutController::class)->name('logout');
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
