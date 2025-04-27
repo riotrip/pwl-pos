a511eb45e1bedb7736d78ba94d1f60c3f09420b2
Prayogo <riotriprayogo31@gmail.com>
2025-04-28 05:53:58 +0700

    fix: correct route parameters for BarangController to use 'barang' instead of 'kategori'

--- routes/api.php
+++ routes/api.php
@@ -46,9 +46,9 @@ Route::delete('/kategories/{kategori}', [KategoriController::class, 'destroy']);
 
 Route::get('/barangs', [BarangController::class, 'index']);
 Route::post('/barangs', [BarangController::class, 'store']);
-Route::get('/barangs/{kategori}', [BarangController::class, 'show']);
-Route::put('/barangs/{kategori}', [BarangController::class, 'update']);
-Route::delete('/barangs/{kategori}', [BarangController::class, 'destroy']);
+Route::get('/barangs/{barang}', [BarangController::class, 'show']);
+Route::put('/barangs/{barang}', [BarangController::class, 'update']);
+Route::delete('/barangs/{barang}', [BarangController::class, 'destroy']);
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
